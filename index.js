585856f8ba4e6995510751205ae9fd4d4e3a8c59
arafatshabbir2 <110466740+arafatshabbir2@users.noreply.github.com>
2023-11-27 09:31:23 +0600

    added payment intent api for payment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -19,6 +20,21 @@ app.use(
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t245pno.mongodb.net/?retryWrites=true&w=majority`;
+
+const tokenVerify = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "Unauthorized Access Request" });
+  }
+  const token = req?.headers?.authorization?.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Unauthorized access Detected" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -72,9 +88,9 @@ async function run() {
       const result = await propertiesCollection.find().toArray();
       res.send(result);
     });
-// get the properties that are verified
+    // get the properties that are verified
     app.get("/api/getVerifiedProperties", async (req, res) => {
-      const query = {propertyVerificationStatus:"verified"}
+      const query = { propertyVerificationStatus: "verified" };
       const result = await propertiesCollection.find(query).toArray();
       res.send(result);
     });
@@ -87,7 +103,7 @@ async function run() {
       console.log(result);
       res.send(result);
     });
-    // get singleProduct
+    // get signleProperty
     app.get("/api/detailProperty/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -101,6 +117,7 @@ async function run() {
       const result = await wishCollection.find(query).toArray();
       res.send(result);
     });
+    // get wishlist for make an offer
     app.get("/api/getWish/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -114,24 +131,88 @@ async function run() {
       const result = await offersCollection.find(query).toArray();
       res.send(result);
     });
+    // get requested property for agent
     app.get("/api/getRequestedProperty", async (req, res) => {
       const email = req.query.email;
       const query = { agentEmail: email };
       const result = await offersCollection.find(query).toArray();
       res.send(result);
     });
+    // Get a specific offer data for Payment
+    app.get("/api/getOfferData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await offersCollection.findOne(query);
+      res.send(result);
+    });
+    // Get sold properties for agent
+    app.get("/api/getSoldProperties", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        agentEmail: email,
+        payment: "completed",
+      };
+      const result = await offersCollection.find(query).toArray()
+      res.send(result)
+    });
+    // make a token for successfully user login
+    app.post("/api/user/accessToken", async (req, res) => {
+      try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        console.log(req.body, token);
+        res.send({ token });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    // Create Payment Intenet For User Pay
+    app.post("/api/createPaymentIntent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+    // update offer status
+    app.patch("/api/updateOfferStatus/:id", async (req, res) => {
+      const updatedData = req.body;
+      console.log(updatedData.status);
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateStatus = {
+        $set: {
+          offerStatus: updatedData.status,
+          transactionId: updatedData.transactionId,
+          payment: updatedData.payment,
+        },
+      };
+      const result = await offersCollection.updateOne(
+        query,
+        updateStatus,
+        options
+      );
+      res.send(result);
+    });
     // create user
     app.post("/api/users", async (req, res) => {
       try {
         const userData = req.body;
-        console.log(userData);
         const email = req.query.email;
         const query = { email: email };
         const existUser = await userCollection.findOne(query);
         if (existUser) {
           return res.send({ message: "user Already Exist" });
         }
-        console.log(req.body);
         const result = await userCollection.insertOne(userData);
         res.send(result);
       } catch (error) {
@@ -143,7 +224,6 @@ async function run() {
       const wishlist = req.body;
       const result = await wishCollection.insertOne(wishlist);
       res.send(result);
-      console.log(wishlist, result);
     });
     // add offer to offercollection
     app.post("/api/addOffer", async (req, res) => {
@@ -167,7 +247,6 @@ async function run() {
     app.patch("/api/changeRole/:id", async (req, res) => {
       const id = req.params.id;
       const newrole = req.query.role;
-      console.log(newrole);
       const query = { _id: new ObjectId(id) };
       const updatedRole = {
         $set: {
@@ -182,16 +261,52 @@ async function run() {
       const id = req.params.id;
       const newrole = req.query.role;
       const email = req.query.email;
-      console.log(newrole);
       const query = { _id: new ObjectId(id) };
       const updatedRole = {
         $set: {
           role: newrole,
         },
       };
-      const filter = {agentEmail: email};
+      const filter = { agentEmail: email };
       const result = await userCollection.updateOne(query, updatedRole);
-      const result2 = await propertiesCollection.deleteMany(filter)
+      const result2 = await propertiesCollection.deleteMany(filter);
+      res.send(result);
+    });
+    // Update the Property Verification status Based On admin Action
+    app.patch("/api/updateStatus/:id", async (req, res) => {
+      const status = req.query.status;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateStatus = {
+        $set: {
+          propertyVerificationStatus: status,
+        },
+      };
+      const result = await propertiesCollection.updateOne(query, updateStatus);
+      res.send(result);
+    });
+    // Update a OfferStatus For agent
+    app.patch("/api/updateOffer/:id", async (req, res) => {
+      const status = req.query.status;
+      const propertyId = req.query.propertyId;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const acceptOne = {
+        $set: {
+          offerStatus: status,
+        },
+      };
+      const filter = {
+        _id: { $ne: new ObjectId(id) },
+        propertyId: propertyId,
+      };
+      const rejectOther = {
+        $set: {
+          offerStatus: "rejected",
+        },
+      };
+      const result = await offersCollection.updateOne(query, acceptOne);
+      const result2 = await offersCollection.updateMany(filter, rejectOther);
       res.send(result);
     });
     // update agent Property
@@ -211,7 +326,7 @@ async function run() {
       res.send(result);
     });
 
-    // update a verification status based on admin response 
+    // update a verification status based on admin response
 
     // delete agent property
     app.delete("/api/delete-property/:id", async (req, res) => {
