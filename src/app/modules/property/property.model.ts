2bc8a1c2eed3263c125b3ae129ace6a09359508d
arafat-sabbir <arafatshabbir8@gmail.com>
2024-10-21 00:30:02 +0600

    Create Property Model

--- src/app/modules/property/property.model.ts
+++ src/app/modules/property/property.model.ts
@@ -4,15 +4,17 @@ import { TProperty } from './property.interface';
 // Define an interface representing a Property document
 
 // Define the Property schema
-const PropertySchema: Schema<TProperty> = new Schema({
-  // Define schema fields here
-  // Example fields (replace with actual schema)
-  // fieldName: {
-  //   type: Schema.Types.FieldType,
-  //   required: true,
-  //   trim: true,
-  // },
-},{timestamps:true,versionKey:false});
+const PropertySchema: Schema<TProperty> = new Schema(
+  {
+    title: String,
+    verificationStatus: { type: String, default: 'pending' },
+    minPrice: Number,
+    maxPrice: Number,
+    advertiseStatus: { type: String, default: 'no' },
+    agent: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 // Create the Property model
 const PropertyModel = mongoose.model<TProperty>('Property', PropertySchema);
