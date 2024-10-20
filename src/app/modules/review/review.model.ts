b562aab97778b6d83b3307d400f0d3dd8103fd29
arafat-sabbir <arafatshabbir8@gmail.com>
2024-10-21 00:27:16 +0600

    Create Review Model

--- src/app/modules/review/review.model.ts
+++ src/app/modules/review/review.model.ts
@@ -4,18 +4,18 @@ import { TReview } from './review.interface';
 // Define an interface representing a Review document
 
 // Define the Review schema
-const ReviewSchema: Schema<TReview> = new Schema({
-  // Define schema fields here
-  // Example fields (replace with actual schema)
-  // fieldName: {
-  //   type: Schema.Types.FieldType,
-  //   required: true,
-  //   trim: true,
-  // },
-},{timestamps:true,versionKey:false});
+const ReviewSchema: Schema<TReview> = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    review: String,
+    propertyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 // Create the Review model
 const ReviewModel = mongoose.model<TReview>('Review', ReviewSchema);
 
 // Export the Review model
 export default ReviewModel;
+
