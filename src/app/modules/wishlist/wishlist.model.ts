34ecdcd2ae74ca8bed1883561d7920e68b923158
arafat-sabbir <arafatshabbir8@gmail.com>
2024-10-21 00:29:32 +0600

    create Wishlist Model

--- src/app/modules/wishlist/wishlist.model.ts
+++ src/app/modules/wishlist/wishlist.model.ts
@@ -4,15 +4,13 @@ import { TWishlist } from './wishlist.interface';
 // Define an interface representing a Wishlist document
 
 // Define the Wishlist schema
-const WishlistSchema: Schema<TWishlist> = new Schema({
-  // Define schema fields here
-  // Example fields (replace with actual schema)
-  // fieldName: {
-  //   type: Schema.Types.FieldType,
-  //   required: true,
-  //   trim: true,
-  // },
-},{timestamps:true,versionKey:false});
+const WishlistSchema: Schema<TWishlist> = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 // Create the Wishlist model
 const WishlistModel = mongoose.model<TWishlist>('Wishlist', WishlistSchema);
