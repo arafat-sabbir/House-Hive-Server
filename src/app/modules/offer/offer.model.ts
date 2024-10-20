51b33cdeb090b09ffcbeb1ee62e29772a018b7cf
arafat-sabbir <arafatshabbir8@gmail.com>
2024-10-21 00:28:01 +0600

    Create OfferModel

--- src/app/modules/offer/offer.model.ts
+++ src/app/modules/offer/offer.model.ts
@@ -4,15 +4,16 @@ import { TOffer } from './offer.interface';
 // Define an interface representing a Offer document
 
 // Define the Offer schema
-const OfferSchema: Schema<TOffer> = new Schema({
-  // Define schema fields here
-  // Example fields (replace with actual schema)
-  // fieldName: {
-  //   type: Schema.Types.FieldType,
-  //   required: true,
-  //   trim: true,
-  // },
-},{timestamps:true,versionKey:false});
+const OfferSchema: Schema<TOffer> = new Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' },
+    offerStatus: { type: String, default: 'pending' },
+    transactionId: String,
+    payment: Number,
+  },
+  { timestamps: true, versionKey: false }
+);
 
 // Create the Offer model
 const OfferModel = mongoose.model<TOffer>('Offer', OfferSchema);
